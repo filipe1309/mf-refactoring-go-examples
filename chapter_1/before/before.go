59f6890a7a73176b276624df58d9756ee6680cf5
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-06-25 00:11:57 -0300

    chore: add deps

--- chapter_1/before/before.go
+++ chapter_1/before/before.go
@@ -21,103 +21,103 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "golang.org/x/text/currency"
-  "math"
-  "os"
-  "strings"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/text/currency"
+	"math"
+	"os"
+	"strings"
 )
 
 type Invoice struct {
-  Customer     string        `json:"customer"`
-  Performances []Performance `json:"performances"`
+	Customer     string        `json:"customer"`
+	Performances []Performance `json:"performances"`
 }
 
 type Performance struct {
-  PlayID   string `json:"playID"`
-  Audience int    `json:"audience"`
+	PlayID   string `json:"playID"`
+	Audience int    `json:"audience"`
 }
 
 type Play struct {
-  Name string `json:"name"`
-  Type string `json:"type"`
+	Name string `json:"name"`
+	Type string `json:"type"`
 }
 
 func formatUSD(amount float64) string {
-  return fmt.Sprintf("%+v", currency.USD.Amount(amount))
+	return fmt.Sprintf("%+v", currency.USD.Amount(amount))
 }
 
 func main() {
-  playsFile, err := os.ReadFile("plays.json")
-  if err != nil {
-    fmt.Println(err)
-  }
+	playsFile, err := os.ReadFile("plays.json")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-  var plays map[string]Play
-  if err := json.Unmarshal(playsFile, &plays); err != nil {
-    fmt.Println(err)
-  }
+	var plays map[string]Play
+	if err := json.Unmarshal(playsFile, &plays); err != nil {
+		fmt.Println(err)
+	}
 
-  invoiceFile, err := os.ReadFile("invoices.json")
-  if err != nil {
-    fmt.Println(err)
-  }
+	invoiceFile, err := os.ReadFile("invoices.json")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-  var invoice Invoice
-  if err := json.Unmarshal(invoiceFile, &invoice); err != nil {
-    fmt.Println(err)
-  }
+	var invoice Invoice
+	if err := json.Unmarshal(invoiceFile, &invoice); err != nil {
+		fmt.Println(err)
+	}
 
-  result, err := statement(invoice, plays)
-  if err != nil {
-    fmt.Println(err)
-  }
+	result, err := statement(invoice, plays)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-  fmt.Println(result)
+	fmt.Println(result)
 }
 
 func statement(invoice Invoice, plays map[string]Play) (string, error) {
-  totalAmount := 0
-  volumeCredits := 0
-  var result strings.Builder
-  result.WriteString(fmt.Sprintf("Statement for %s\n", invoice.Customer))
+	totalAmount := 0
+	volumeCredits := 0
+	var result strings.Builder
+	result.WriteString(fmt.Sprintf("Statement for %s\n", invoice.Customer))
 
-  for _, perf := range invoice.Performances {
-    play := plays[perf.PlayID]
-    thisAmount := 0
+	for _, perf := range invoice.Performances {
+		play := plays[perf.PlayID]
+		thisAmount := 0
 
-    switch play.Type {
-    case "tragedy":
-      thisAmount = 40000
-      if perf.Audience > 30 {
-        thisAmount += 1000 * (perf.Audience - 30)
-      }
-    case "comedy":
-      thisAmount = 30000
-      if perf.Audience > 20 {
-        thisAmount += 10000 + 500*(perf.Audience-20)
-      }
-      thisAmount += 300 * perf.Audience
+		switch play.Type {
+		case "tragedy":
+			thisAmount = 40000
+			if perf.Audience > 30 {
+				thisAmount += 1000 * (perf.Audience - 30)
+			}
+		case "comedy":
+			thisAmount = 30000
+			if perf.Audience > 20 {
+				thisAmount += 10000 + 500*(perf.Audience-20)
+			}
+			thisAmount += 300 * perf.Audience
 
-    default:
-      return "", fmt.Errorf("error: unknown performance type %s", play.Type)
-    }
+		default:
+			return "", fmt.Errorf("error: unknown performance type %s", play.Type)
+		}
 
-    // add volume credits
-    volumeCredits += int(math.Max(float64(perf.Audience)-30, 0))
+		// add volume credits
+		volumeCredits += int(math.Max(float64(perf.Audience)-30, 0))
 
-    // add extra credit for every ten comedy attendees
-    if play.Type == "comedy" {
-      volumeCredits += int(math.Floor(float64(perf.Audience) / 5))
-    }
+		// add extra credit for every ten comedy attendees
+		if play.Type == "comedy" {
+			volumeCredits += int(math.Floor(float64(perf.Audience) / 5))
+		}
 
-    // print line for this order
-    result.WriteString(fmt.Sprintf("%s: %s (%d seats) \n", play.Name, formatUSD(float64(thisAmount/100)), perf.Audience))
-    totalAmount += thisAmount
-  }
+		// print line for this order
+		result.WriteString(fmt.Sprintf("%s: %s (%d seats) \n", play.Name, formatUSD(float64(thisAmount/100)), perf.Audience))
+		totalAmount += thisAmount
+	}
 
-  result.WriteString(fmt.Sprintf("Amount owed is %s\n", formatUSD(float64(totalAmount)/100)))
-  result.WriteString(fmt.Sprintf("You earned %d credits\n", volumeCredits))
-  return result.String(), nil
+	result.WriteString(fmt.Sprintf("Amount owed is %s\n", formatUSD(float64(totalAmount)/100)))
+	result.WriteString(fmt.Sprintf("You earned %d credits\n", volumeCredits))
+	return result.String(), nil
 }
