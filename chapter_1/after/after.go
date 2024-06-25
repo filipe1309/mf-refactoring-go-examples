8c565f279d9946878546bd13f378ca4b36282c3d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-06-24 23:21:15 -0300

    refactor(chapter-1): improve readability by renaming func formatUSD to usd

--- chapter_1/after/after.go
+++ chapter_1/after/after.go
@@ -44,7 +44,7 @@ type Play struct {
   Type string `json:"type"`
 }
 
-func formatUSD(amount float64) string {
+func usd(amount float64) string {
   return fmt.Sprintf("%+v", currency.USD.Amount(amount))
 }
 
@@ -82,11 +82,11 @@ func statement(invoice Invoice) (string, error) {
     volumeCredits += volumeCreditsFor(perf)
 
     // print line for this order
-    result.WriteString(fmt.Sprintf("%s: %s (%d seats) \n", playFor(perf).Name, formatUSD(float64(thisAmount/100)), perf.Audience))
+    result.WriteString(fmt.Sprintf("%s: %s (%d seats) \n", playFor(perf).Name, usd(float64(thisAmount/100)), perf.Audience))
     totalAmount += thisAmount
   }
 
-  result.WriteString(fmt.Sprintf("Amount owed is %s\n", formatUSD(float64(totalAmount)/100)))
+  result.WriteString(fmt.Sprintf("Amount owed is %s\n", usd(float64(totalAmount)/100)))
   result.WriteString(fmt.Sprintf("You earned %d credits\n", volumeCredits))
   return result.String(), nil
 }
