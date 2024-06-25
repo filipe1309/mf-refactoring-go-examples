16173fb5ef51d97c790b82f55478cfdf59e53208
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-06-24 22:20:45 -0300

    refactor(chapter-1): move amount calculation logic for different play types to another function using Extract Function strategy

--- chapter_1/after/after.go
+++ chapter_1/after/after.go
@@ -85,23 +85,10 @@ func statement(invoice Invoice, plays map[string]Play) (string, error) {
 
   for _, perf := range invoice.Performances {
     play := plays[perf.PlayID]
-    thisAmount := 0
 
-    switch play.Type {
-    case "tragedy":
-      thisAmount = 40000
-      if perf.Audience > 30 {
-        thisAmount += 1000 * (perf.Audience - 30)
-      }
-    case "comedy":
-      thisAmount = 30000
-      if perf.Audience > 20 {
-        thisAmount += 10000 + 500*(perf.Audience-20)
-      }
-      thisAmount += 300 * perf.Audience
-
-    default:
-      return "", fmt.Errorf("error: unknown performance type %s", play.Type)
+    thisAmount, err := amountFor(perf, play)
+    if err != nil {
+      return "", err
     }
 
     // add volume credits
@@ -120,4 +107,27 @@ func statement(invoice Invoice, plays map[string]Play) (string, error) {
   result.WriteString(fmt.Sprintf("Amount owed is %s\n", formatUSD(float64(totalAmount)/100)))
   result.WriteString(fmt.Sprintf("You earned %d credits\n", volumeCredits))
   return result.String(), nil
+}
+
+func amountFor(perf Performance, play Play) (int, error) {
+  var thisAmount int
+
+  switch play.Type {
+    case "tragedy":
+      thisAmount = 40000
+      if perf.Audience > 30 {
+        thisAmount += 1000 * (perf.Audience - 30)
+      }
+    case "comedy":
+      thisAmount = 30000
+      if perf.Audience > 20 {
+        thisAmount += 10000 + 500*(perf.Audience-20)
+      }
+      thisAmount += 300 * perf.Audience
+
+    default:
+      return thisAmount, fmt.Errorf("error: unknown performance type %s", play.Type)
+  }
+
+  return thisAmount, nil
 }
