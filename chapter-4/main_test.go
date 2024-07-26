ce66180fd528d67bf57d2b50eac16cbb1ead05dc
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-26 16:37:25 -0300

    test(chapter-4): Add unit test for getProfit function

--- chapter-4/main_test.go
+++ chapter-4/main_test.go
@@ -9,3 +9,8 @@ func TestShortfall(t *testing.T) {
 	asia := sampleProvinceData()
 	assert.Equal(t, 5, asia.getShortfall())
 }
+
+func TestProfit(t *testing.T) {
+	asia := sampleProvinceData()
+	assert.Equal(t, 230, asia.getProfit())
+}
